1ca1f673a878f9ba9858d83a9805aa71be5a0091
syserrano <syserrano@usf.edu>
2026-03-29 23:22:32 -0400

    Function to handle error and called the functions

--- cp_2.js
+++ cp_2.js
@@ -23,4 +23,34 @@ async function fetchProductsAsync() {
         }
     }
 
+function displayProducts(products) {
+    const container = 
+    document.getElementByld("product-container");
 
+    container.innerHTML = "";
+
+    products.slice(0, 5).forEach(product => {
+    const card = document.createElement("div");
+    card.classList.add("product-card");
+    })
+
+    const img = document.createElement("img");
+    img.src = product.image;
+    img.alt = product.name;
+
+    const name = document.createElement("h3")
+    name.textContext = '$${(product.price/100).toFixed(2)}'
+
+    card.appendChild(img);
+    card.appendChild(name);
+    card.appendChild(price);
+
+    container.appendChild(card);
+    }
+
+function handleError(error){
+    console.error(`An error has happened: ${error.message}`);
+}
+
+fetchProductsThen()
+fetchProductsAsync()
